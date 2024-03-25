eb2040a7c7de256b27cc89e3362ffcb67283c825
KapowD <mikael99988@gmail.com>
2024-03-26 00:38:56 +0700

    README

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,82 @@
-export interface IProductItem {
+//SECTION: MODELS
+
+export type Category =
+	| 'софт-скил'
+	| 'другое'
+	| 'дополнительное'
+	| 'кнопка'
+	| 'хард-скил';
+//+
+export interface IItemShort {
 	id: string;
+	title: string;
+	price: number;
+}
+//+
+export interface IItem extends IItemShort {
 	description?: string;
 	image: string;
-	title: string;
-	category: string;
-	price: number | null;
+	category: Category;
+}
+
+export interface IItemCatalogue {
+	items: Set<IItem>;
+	addItem: (item: IItem) => void;
+	addItems: (items: IItem[]) => void;
+	getItemByID: (id: IItem) => IItem;
+	getItems: () => IItem[];
+}
+
+export enum PaymentMethod {
+	ONLINE = 'online',
+	ON_ARRIVAL = 'on_arrival',
+}
+
+// + IOrderForm 
+export interface IFormInfo {
+	paymentMethod: PaymentMethod;
+	address: string;
+	email: string;
+	phone: string;
+}
+// + IOrderForm 
+export interface IOrder extends IFormInfo {
+	items: string[];
+	total: number;
+}
+
+export interface IBasket {
+	items: IItem[];
+	addItem: (item: IItem) => void;
+	removeItemById: (id: number) => void;
+	clearBasket: () => void;
+	getItemsNumber: () => number;
+	getCost: () => number;
+}
+
+export type FormErrors = Partial<Record<keyof IFormInfo, string>>;
+
+export interface IOrderResult {
+	id: string;
+	total: number;
+}
+
+export interface IAppState {
+	catalog: IItemCatalogue[];
+	basket: IBasket;
+	preview: string | null;
+	order: IOrder | null;
+	loading: boolean;
+}
+
+//SECTION: VIEWS
+
+export interface ISuccess{
+    description: string;
+}
+
+export interface IPage{
+	counter: number;
+	catalog: HTMLElement[];
+	locked: boolean;
 }
